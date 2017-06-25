fefdadb60119010147a48f46280bff75a1a203e9
Motherfox.com <admin@motherfoxcoin.com>
2017-06-25 13:55:30 +0100

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "motherfoxcoin")
+set(CN_CURRENCY_DISPLAY_NAME "motherfoxcoin")
+set(CN_CURRENCY_TICKER "motherfoxcoin")
